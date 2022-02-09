264535a497d1133d590702a8756599441545f335
Stewart Macias <macias.stewart@gmail.com>
2022-02-09 09:07:33 -0500

    function composition

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
+/* {
+  tag: 'h1',
+  attr: {
+    class:'title'
+  }
+} */
+
+const attrsToString = (obj ={}) => {
+  const keys = Object.keys(obj);
+  const attrs = [];
+
+  for(let i=0; i< keys.length; i++) {
+    let attr = keys[i];
+    attrs.push(`${attr}="${obj[attr]}"`)
+  }
+
+  const string = attrs.join('');
+  return string;
+}
+
+const tag = t => content => `<${t}> ${content} </${t}>`;
+
 let description = $('#description');
 let carbs = $('#carbs');
 let calories = $('#calories');
